d1dd81f29aa3edad5079dcb224b0bea29e423039
HoppaySama <113163196+Hoppay-sama@users.noreply.github.com>
2024-11-24 22:36:48 +0800

    Delete Ecstatic directory

--- Ecstatic/Album/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Album(models.Model):
-    image = models.ImageField(default='0.png', blank=True)
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    date = models.DateField()
